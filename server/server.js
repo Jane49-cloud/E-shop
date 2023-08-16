afa74a15620c300e3acc2ec1a91860fcbfda350a
Jane49-cloud <janendirangu49@gmail.com>
2023-08-16 12:09:42 +0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`server listening on port ${port}...`));
